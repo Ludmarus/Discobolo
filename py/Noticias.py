33dd0f1c6e43090f605b05f6be01024c0e8f6af6
Enrique <102213686+Kiketudo@users.noreply.github.com>
2022-12-23 20:39:27 +0100

    finalizar Noticias.py

--- py/Noticias.py
+++ py/Noticias.py
@@ -7,3 +7,15 @@ from IPython.display import HTML
 rows = "\n".join(["<tr><td>{0}</td></tr>".format(desc)
                   for desc in descriptions])
 HTML("<table>"+rows+"</table>")
+response = requests.get(search_url, headers=headers, params=params)
+response.raise_for_status()
+search_results = json.dumps(response.json())
+
+escriptions = [article["description"] for article in search_results["value"]]
+
+from IPython.display import HTML
+rows = "\n".join(["<tr><td>{0}</td></tr>".format(desc)
+                  for desc in descriptions])
+HTML("<table>"+rows+"</table>")
+
+
